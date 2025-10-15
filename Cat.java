6efcd203d5a9549ada809ffd7b18d915835f8e61
Alan Ruelas-Cordova <ruelascordA@cwu.edu>
2025-10-15 10:46:50 -0700

    Added cat file

---
+++ Cat.java
@@ -0,0 +1,43 @@
+/**
+ * @Author Alan Ruelas-Cordova
+ * Date: 10/15/25 
+ * Harrison
+ * Cat.java
+ */
+
+public class Cat extends Animal{
+
+    /**
+     * Contructor of the Cat Object
+     * @param name
+     */
+    public Cat(String name){
+        super(name);
+    }
+
+    /**
+     * The sound method that makes the cat object MEOW
+     */
+    public void sound(){
+        System.out.println("MEEEOW");
+    }
+
+    /**
+     * The play method that makes the cat play with String.
+     */
+    public void play(){
+        System.out.println(this.name + " is playing with the string!!!");
+    }
+
+    /**
+     * The eat method that makes the cat eat some fish
+     */
+    public void eat(){
+        System.out.println(this.name + "ate some fish!");
+    }
+
+
+
+
+
+}
