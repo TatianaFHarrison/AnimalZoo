3564606d36516efae1684cfab25325b88921cebe
Alan Ruelas-Cordova <ruelascordA@cwu.edu>
2025-10-15 10:53:56 -0700

    Alan

--- Chipmunk.java
+++ Chipmunk.java
@@ -30,12 +30,3 @@ public class Chipmunk extends Animal {
     }
 
 }
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Chipmunk");
-        
-        Chipmunk chipmunk = new Chipmunk("Tony Tran");
-        chipmunk.play();
-        chipmunk.sound();
-    }
-}
